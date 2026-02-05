1fcec1f7682fd151547b46d1d55dfec6067884dc
archanaunni-eloor <archanaunni@gmail.com>
2026-02-05 18:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 model = joblib.load('attrition_model.pkl')
 model_columns = joblib.load('model_columns.pkl')
 
-st.title("Emplyee Attrition Prediction System (87.76% Accuracy) by Unni R")
+st.title("Employee Attrition Prediction System (87.76% Accuracy) by Unni R")
 st.write("Enter Employee details to predict Attrition trend.")
 
 
