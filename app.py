258da12623ec4bde0e2b2656ea16b307c0775a8f
archanaunni-eloor <archanaunni@gmail.com>
2026-02-05 14:25:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+
+model = joblib.load('attrition_model.pkl')
+model_columns = joblib.load('model_columns.pkl')
+
+st.title("IBM HR Attrition Prediction System")
+st.write("Enter Employee details to predict Attrition trend.")
+
+
+age = st.slider("Age", 18, 60, 30)
+income = st.number_input("Monthly Income", min_value=1000, max_value=20000, value=5000)
+overtime = st.selectbox("Overtime", ["Yes", "No"])
+job_level = st.slider("Job Level", 1, 5, 2)
+
+
+if st.button("Predict"):
+    
+    data = pd.DataFrame([[age, income, job_level]], columns=['Age', 'MonthlyIncome', 'JobLevel'])
+    
+   
+    for col in model_columns:
+        if col not in data.columns:
+            data[col] = 0
+            
+    prediction = model.predict(data[model_columns])[0]
+    
+    if prediction == 1:
+        st.error("☹️ High Risk: This employee is likely to leave.")
+    else:
+        st.success("🙂 Low Risk: This employee is likely to stay.")
+        
